bfaaed981b228b5e06f71994c94de86f72db9c5d
Arlen <arlen.pena@gmail.com>
2020-08-09 19:33:48 -0500

    Initial Commit

---
+++ internationalizationBaseFinish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  internationalizationBaseFinish
+//
+//  Created by Arlen Peña on 09/08/20.
+//  Copyright © 2020 Arlen Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
